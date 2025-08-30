75ac0bf471f6f45294c8e156a3e1c99b4996cb83
kelvin nkwor <nkworkelvin007@gmail.com>
2025-08-30 07:20:54 +0100

    implemented the styling of featured product and subscription feature

--- script.js
+++ script.js
@@ -127,8 +127,8 @@ async function getFeaturedProducts() {
                             <img 
                                 src="${product.image}" 
                                 alt="${product.title}"
-                                class="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105 p-0"
-                                
+                                class="h-screen w-screen object-cover transition-transform duration-300 group-hover:scale-105 p-0"
+                                width  
                             />
                         </div>
                     </div>
@@ -153,14 +153,17 @@ async function getFeaturedProducts() {
                         </div>
                     </div>
 
-                    <div class="flex items-center justify-between mt-4">
-                        <span class="text-2xl font-bold text-blue-600">
+                    <div class="p-4 pt-0 flex items-center justify-between">
+                        <span class="text-2xl font-bold text-primary">
                             $${product.price.toFixed(2)}
                         </span>
                         <button 
                             onclick="alert('Added ${product.title} to cart!')" 
-                            class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+                            class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 inline-flex items-center justify-center gap-2"
                         >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white h-5 w-5 mr-2">
+                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                            </svg>
                             Add to Cart
                         </button>
                     </div>
